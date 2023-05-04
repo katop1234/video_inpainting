eec8fc79d67aa2e72ba932519e23fd21e7e06939
katop1234 <kathan@berkeley.edu>
2023-05-04 19:42:05 +0000

    made dataset_root work from any home dir easily through ~

--- video_mae_code/main_pretrain.py
+++ video_mae_code/main_pretrain.py
@@ -231,7 +231,7 @@ def get_args_parser():
     parser.add_argument("--cls_embed", action="store_true")
     parser.set_defaults(cls_embed=True)
 
-    parser.add_argument("--dataset_root", default="/home/amir/Datasets", help="parent folder for all datasets")
+    parser.add_argument("--dataset_root", default=os.path.join(os.path.expanduser("~"), "Datasets"), help="parent folder for all datasets")
     parser.add_argument('--image_dataset_list', nargs='+', default=['cvf'])
     parser.add_argument('--image_dataset_conf', nargs='+', default=[1])
     parser.add_argument('--video_dataset_list', nargs='+', default=['kinetics'])
