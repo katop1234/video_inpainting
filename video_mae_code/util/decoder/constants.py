fixed tensor normalize to broadcast to any size tensor as long as a 3 is detected

video_mae_code/util/decoder/constants.py
@@ -1,6 +1,6 @@
 import torch
 
 ### CONSTS
-mean = torch.tensor([0.485, 0.456, 0.406])
-std = torch.tensor([0.229, 0.224, 0.225])
+mean = torch.tensor([0.45, 0.45, 0.45])
+std = torch.tensor([0.225, 0.225, 0.225])
 ### CONSTS

video_mae_code/util/decoder/utils.py
@@ -230,36 +230,36 @@ def load_image_lists(frame_list_file, prefix="", return_list=False):
     return dict(image_paths), dict(labels)
 
 def tensor_normalize(tensor):
-    """
-    Normalize a given tensor by subtracting the mean and dividing the std.
-    Args:
-        tensor (tensor): tensor to normalize.
-        mean (tensor or list): mean value to subtract.
-        std (tensor or list): std to divide.
-    """
-    
     tensor = tensor.float()
     mean = constants.mean.to(tensor.device)
     std = constants.std.to(tensor.device)
-    
+
     tensor = tensor / 255.0
-    tensor = tensor - mean
-    tensor = tensor / std
+
+    # Get the position of the color channel dimension
+    color_dim = tensor.shape.index(3)
+
+    # Expand the mean and std tensors to match the tensor shape
+    expanded_mean = mean.view(*([1] * color_dim), 3, *([1] * (len(tensor.shape) - color_dim - 1)))
+    expanded_std = std.view(*([1] * color_dim), 3, *([1] * (len(tensor.shape) - color_dim - 1)))
+
+    tensor = tensor - expanded_mean
+    tensor = tensor / expanded_std
     return tensor
 
 def revert_tensor_normalize(tensor):
-    """
-    Revert normalization for a given tensor by multiplying by the std and adding the mean.
-    Args:
-        tensor (tensor): tensor to revert normalization.
-        mean (tensor or list): mean value to add.
-        std (tensor or list): std to multiply.
-    """
     tensor = tensor.float()
     mean = constants.mean.to(tensor.device)
     std = constants.std.to(tensor.device)
-    
-    tensor = tensor * std + mean
+
+    # Get the position of the color channel dimension
+    color_dim = tensor.shape.index(3)
+
+    # Expand the mean and std tensors to match the tensor shape
+    expanded_mean = mean.view(*([1] * color_dim), 3, *([1] * (len(tensor.shape) - color_dim - 1)))
+    expanded_std = std.view(*([1] * color_dim), 3, *([1] * (len(tensor.shape) - color_dim - 1)))
+
+    tensor = tensor * expanded_std + expanded_mean
     tensor = torch.clip(tensor * 255, 0, 255)
     return tensor
 