1b0b1be7a3b0275d3aa053a7130337c500be71ea
katop1234 <kathan@berkeley.edu>
2023-05-22 16:40:14 +0000

    made it cleaner

--- video_mae_code/util/eval.py
+++ video_mae_code/util/eval.py
@@ -256,10 +256,10 @@ def visualize_video_prompting(model, input_video_viz_dir="test_cases/final_tempo
     num_patches = 14
     y = test_model_output.argmax(dim=-1)
     im_paste, mask, orig_image = decode_raw_prediction(mask, model, num_patches, test_model_input, y)
-    im_paste = im_paste.permute((0, 1, 4, 2, 3))
+    
+    im_paste = im_paste.permute((0, 1, 4, 2, 3)).squeeze(0).permute(1, 0, 3, 2).unsqueeze(0)
+    im_paste = im_paste.cpu().numpy().astype(np.uint8)
 
-    im_paste = im_paste.squeeze(0).permute(1, 0, 3, 2).unsqueeze(0)
-    im_paste = (im_paste.cpu().numpy()).astype(np.uint8)
 
     wandb_video_object = wandb.Video(
         data_or_path=im_paste,
