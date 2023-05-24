781422f352374a8d8ddd7b5da32d0525ca3a37b7
katop1234 <kathan@berkeley.edu>
2023-05-24 07:00:18 +0000

    moving to new branch

--- video_mae_code/dataset_factory.py
+++ video_mae_code/dataset_factory.py
@@ -6,13 +6,32 @@ from torchvision.transforms import transforms
 from util.decoder import constants
 from util.kinetics import Kinetics
 
+class VideoDataset(Kinetics):
+    def __init__(self, path_to_data_dir, 
+                 mode="pretrain", 
+                 sampling_rate=4, 
+                 num_frames=16, 
+                 train_jitter_scales=(256, 320), 
+                 repeat_aug=1, 
+                 jitter_aspect_relative=[0.75, 1.3333], 
+                 jitter_scales_relative=[0.5, 1.0]):
+        
+        super().__init__(path_to_data_dir, 
+                         mode=mode, 
+                         sampling_rate=sampling_rate, 
+                         num_frames=num_frames, 
+                         train_jitter_scales=train_jitter_scales, 
+                         repeat_aug=repeat_aug, 
+                         jitter_aspect_relative=jitter_aspect_relative, 
+                         jitter_scales_relative=jitter_scales_relative)
+
+
 def get_image_transforms():
     return transforms.Compose([
         transforms.RandomResizedCrop(224, scale=(0.2, 1.0), interpolation=3),  # 3 is bicubic
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=constants.mean, std=constants.std)])
-
 
 def get_dataset(name, root_path, ds_type):
     if ds_type == 'image':
@@ -38,14 +57,16 @@ def get_dataset(name, root_path, ds_type):
                 jitter_aspect_relative=[0.75, 1.3333],
                 jitter_scales_relative=[0.5, 1.0],
                 )
+        elif name == "Objectron":
+            dataset_train = VideoDataset(path_to_data_dir="/shared/katop1234/Datasets/Objectron")
+        elif name == "CrossTask":
+            dataset_train = VideoDataset(path_to_data_dir="/shared/katop1234/Datasets/CrossTask")
         else:
             raise NotImplementedError()
-
     else:
         raise ValueError("Wrong dataset type.")
 
     return dataset_train
-
 
 class MergedDataset(torch.utils.data.Dataset):
     def __init__(self, root_path, image_dataset_list, image_dataset_conf, video_dataset_list, video_dataset_conf,
@@ -66,7 +87,7 @@ class MergedDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         # return 16 # For testing purposes
-        return (2 ** 14) * 5 # 2^14 * 5 = 81920, which is around CVF size
+        return 79000
 
     def __getitem__(self, index: int):
         print("getting output from getitem", "index", index, "len(self.datasets)", len(self.datasets))
