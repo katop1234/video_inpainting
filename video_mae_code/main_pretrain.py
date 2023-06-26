a5c484805b0dab80edf2cbc696e1791655ecebc6
Kathan Shah kathan@berkeley.edu <kathan@berkeley.edu>
2023-06-26 17:53:09 +0000

    WITH CORRECT WANDB ID

--- video_mae_code/main_pretrain.py
+++ video_mae_code/main_pretrain.py
@@ -396,7 +396,7 @@ def main(args):
         base_lr = (args.lr * 256 / eff_batch_size)
         wandb_config['base_lr'] = base_lr
         wandb.init(
-            #resume=,
+            resume="xc77oujt",
             project="video_inpainting2",
             config=wandb_config)
 
