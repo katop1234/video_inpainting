7737a84a38dc458baed082a1446aae6c842d5d6c
Kathan Shah kathan@berkeley.edu <kathan@berkeley.edu>
2023-06-27 06:58:55 +0000

    IMAGE ONLY RIN

--- video_mae_code/main_pretrain.py
+++ video_mae_code/main_pretrain.py
@@ -396,7 +396,7 @@ def main(args):
         base_lr = (args.lr * 256 / eff_batch_size)
         wandb_config['base_lr'] = base_lr
         wandb.init(
-            resume="pv45spyh",
+            resume="hwhsdrc0",
             project="video_inpainting2",
             config=wandb_config)
 
