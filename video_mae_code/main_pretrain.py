12b850d338e033a648e65955d6933aa18fdf69ef
Kathan Shah kathan@berkeley.edu <kathan@berkeley.edu>
2023-06-30 01:54:25 +0000

    running on chary with lower lr

--- video_mae_code/main_pretrain.py
+++ video_mae_code/main_pretrain.py
@@ -397,7 +397,7 @@ def main(args):
         base_lr = (args.lr * 256 / eff_batch_size)
         wandb_config['base_lr'] = base_lr
         wandb.init(
-            resume="hwhsdrc0",
+            resume="9p4cp96z",
             project="video_inpainting2",
             config=wandb_config)
     
