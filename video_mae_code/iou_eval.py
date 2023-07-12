77eef5efedb0e1486dba1d150929669924e87d76
DannyTran123 <trandanny797@gmail.com>
2023-07-12 18:32:55 +0000

    iou_eval

--- video_mae_code/iou_eval.py
+++ video_mae_code/iou_eval.py
@@ -16,7 +16,7 @@ import evaluate
 #Constants
 color_palette = [
     0,   0,   0,    # Index 0: Black
-    128, 0,   0,    # Index 1: Red
+    255,255,255,    # Index 1: White
 ]
 single_object_cases = ['blackswan', 'breakdance', 'camel', 'car-roundabout', 'car-shadow', 'cows', 'dance-twirl', 'dog', 'drift-chicane', 'drift-straight', 'goat', 'libby', 'parkour']
 
@@ -88,7 +88,7 @@ def get_results_path(store_path, eval_name):
     results_path = os.path.join(store_path, eval_name)
     return results_path  
 
-def generate_segmentations(model, store_path, prompt_csv, davis_prompt_path, davis_2x2_prompt_path, davis_image_prompt_path):
+def generate_segmentations(model, store_path, single_prompt_csv, prompt_csv, davis_prompt_path, davis_2x2_prompt_path, davis_image_prompt_path):
     if type(model) is torch.nn.parallel.DistributedDataParallel:
         model = model.module
     
@@ -113,37 +113,53 @@ def generate_segmentations(model, store_path, prompt_csv, davis_prompt_path, dav
                 val_end_idx = int(row[7])
                 
                 video_prompt_orig = "DAVIS_{prompt_num}.mp4".format(prompt_num=prompt_num)
+
+                video_prompt_orig = os.path.join(davis_prompt_path, video_prompt_orig)
+                
+                video_im_paste_orig = image_video_generation(video_prompt_orig, model, 'spatiotemporal')
+
+                frames_orig = extract_prompt_seg(video_im_paste_orig[0], val_height, val_width, 'spatiotemporal')
+ 
+                seg_save_orig_path = os.path.join(results_orig_path, val)
+
+                if not os.path.exists(seg_save_orig_path):
+                    os.mkdir(seg_save_orig_path)
+
+                save_segmentations(frames_orig, val, seg_save_orig_path, val_end_idx, 'spatiotemporal')
+                prompt_num += 1
+    
+    with open(single_prompt_csv, 'r') as file:
+        csvreader = csv.reader(file)
+        prompt_num = 0
+        for row in csvreader:
+            if row[0] != 'train':
+                val = row[1]
+                val_height = int(row[4])
+                val_width = int(row[5])
+                val_end_idx = int(row[7])
+                
                 video_prompt_2x2 = "DAVIS_2x2_{prompt_num}.mp4".format(prompt_num=prompt_num)
                 image_prompt_2x2 = "DAVIS_image_{prompt_num}.png".format(prompt_num=prompt_num)
                 
-                video_prompt_orig = os.path.join(davis_prompt_path, video_prompt_orig)
                 video_prompt_2x2 = os.path.join(davis_2x2_prompt_path, video_prompt_2x2)
                 image_prompt_2x2 = os.path.join(davis_image_prompt_path, image_prompt_2x2)
                 
-                video_im_paste_orig = image_video_generation(video_prompt_orig, model, 'spatiotemporal')
                 video_im_paste_2x2 = image_video_generation(video_prompt_2x2, model, '2x2 tube')
                 image_im_paste = image_video_generation(image_prompt_2x2, model, 'test image')
                 
-                frames_orig = extract_prompt_seg(video_im_paste_orig[0], val_height, val_width, 'spatiotemporal')
                 frames_2x2 = extract_prompt_seg(video_im_paste_2x2[0], val_height, val_width, '2x2 tube')
                 frames_image = extract_prompt_seg(image_im_paste[0], val_height, val_width, 'test image')
                 
-                seg_save_orig_path = os.path.join(results_orig_path, val)
                 seg_save_2x2_path = os.path.join(results_2x2_path, val)
                 seg_save_image_path = os.path.join(results_image_path, val)
                 
-                if not os.path.exists(seg_save_orig_path):
-                    os.mkdir(seg_save_orig_path)
                 if not os.path.exists(seg_save_2x2_path):
                     os.mkdir(seg_save_2x2_path)
                 if not os.path.exists(seg_save_image_path):
                     os.mkdir(seg_save_image_path)
-
-                save_segmentations(frames_orig, val, seg_save_orig_path, val_end_idx, 'spatiotemporal')
+                    
                 save_segmentations(frames_2x2, val, seg_save_2x2_path, val_end_idx, '2x2 tube')
                 save_segmentations(frames_image, val, seg_save_image_path, val_end_idx, 'test image')
-                
-                prompt_num += 1
                 
 def image_video_generation(prompt_path, model, mask_test_type):
     test_model_input = get_test_model_input(file=prompt_path)
