7caeca39852631de00a5fa67ffe9b3dd04e46d07
Kathan Shah kathan@berkeley.edu <kathan@berkeley.edu>
2023-07-13 00:19:01 +0000

    updated params

--- video_mae_code/main_pretrain.py
+++ video_mae_code/main_pretrain.py
@@ -399,7 +399,7 @@ def main(args):
         base_lr = (args.lr * 256 / eff_batch_size)
         wandb_config['base_lr'] = base_lr
         wandb.init(
-            resume="191c0ic5",
+            #resume="191c0ic5",
             project="video_inpainting2",
             config=wandb_config)
     
